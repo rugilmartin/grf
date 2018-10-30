cec38813f49ec4191e8fa018a8375a8ef6be9c68
halflearned <halflearned@gmail.com>
2018-10-29 19:42:27 -0700

    Simple argparser. For regression forest only. Untested.

--- core/xcode/xcode/main.cpp
+++ core/xcode/xcode/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  grf_xcode
+//  grf
 //
 //  Created by Vitor Baisi Hadad on 8/6/18.
 //  Copyright © 2018 atheylab. All rights reserved.
@@ -13,13 +13,14 @@
 #include "forest/ForestTrainer.h"
 #include "forest/ForestTrainers.h"
 #include "utilities/ForestTestUtilities.h"
+#include "serialization/ForestSerializer.h"
 
 #include <iostream>
 #include <string>
 
 #include "cxxopts.hpp"
 
-ForestOptions parse_option(cxxopts::ParseResult result) {
+ForestOptions parse_forest_options(cxxopts::ParseResult result) {
     ForestOptions opt(
         result["num_trees"].as<uint>(),
         result["ci_group_size"].as<uint>(),
@@ -35,6 +36,42 @@ ForestOptions parse_option(cxxopts::ParseResult result) {
         0); // samples_per_cluster
     return(opt);
 };
+
+Forest load_forest_from_file(std::string in_serial_name) {
+    std::ifstream infile;
+    infile.open(in_serial_name, std::ios::binary);
+    ForestSerializer forest_loader;
+    Forest forest = forest_loader.deserialize(infile);
+    return(forest);
+}
+
+Forest train_regression_forest(std::unique_ptr<Data> data, ForestOptions forest_options, uint outcome_index) {
+    ForestTrainer trainer = ForestTrainers::regression_trainer(outcome_index);
+    Forest forest = trainer.train(data.get(), forest_options);
+    return(forest);
+}
+
+void save_forest_to_file(std::string filename, Forest forest) {
+    std::ofstream outfile;
+    outfile.open(filename, std::ios::binary);
+    if (!outfile.good()) throw std::runtime_error("Could not open " + filename + ".");
+    ForestSerializer serial;
+    serial.serialize(outfile, forest);
+    outfile.close();
+}
+
+void save_predictions_to_file(std::string filename, std::vector<Prediction> predictions) {
+    std::ofstream outfile;
+    outfile.open(filename);
+    if (!outfile.good()) throw std::runtime_error("Could not open " + filename + ".");
+    for (auto&p: predictions) {
+        auto yhat = p.get_predictions()[0];
+        std::cout << yhat << "\n";
+    }
+    outfile.close();
+}
+
+
 
 
 int main(int argc, char * argv[]) {
@@ -54,37 +91,41 @@ int main(int argc, char * argv[]) {
     ("p,imbalance_penalty", "Imbalance penalty", cxxopts::value<double>()->default_value("0"))
     ("s,random_seed", "Seed", cxxopts::value<uint>()->default_value("12345"))
     ("b,compute_oob_predictions", "Compute OOB predictions", cxxopts::value<bool>()->default_value("true"))
-    ("o,outcome_index", "Outcome index", cxxopts::value<uint>())
-    ("i,input_file", "Input file", cxxopts::value<std::string>())
+    ("y,outcome_index", "Outcome index", cxxopts::value<uint>())
+    ("I,input_file", "Input file", cxxopts::value<std::string>())
+    ("W,write", "Write forest to file", cxxopts::value<std::string>()->default_value("output.forest"))
+    ("L,load", "Load forest from file", cxxopts::value<std::string>()->default_value(""))
+    ("O,output_file", "File that will receive predictions", cxxopts::value<std::string>()->default_value(""))
     ;
 
     auto result = options.parse(argc, argv);
-    ForestOptions forest_options = parse_option(result);
-    std::string input_file = result.count("input_file") ? result["input_file"].as<std::string>() : throw std::runtime_error("Input file is required");
-    uint outcome_index = result.count("outcome_index") ? result["outcome_index"].as<uint>() : throw std::runtime_error("Outcome index is required");
     
-    // Loading data
-    std::cout << "Loading data\n";
-    std::unique_ptr<Data> data(load_data(input_file));
-    
-    std::cout << "Training\n";
-    ForestTrainer trainer = ForestTrainers::regression_trainer(outcome_index);
-    Forest forest = trainer.train(data.get(), forest_options);
-    
-    std::cout << "Predicting\n";
-    ForestPredictor predictor = ForestPredictors::regression_predictor(forest_options.get_num_threads(), forest_options.get_ci_group_size());
-    std::vector<Prediction> predictions = predictor.predict_oob(forest, data.get());
-    
-    std::cout << "Computing MSE\n";
-    double mse = 0;
-    auto obs = forest.get_observations();
-    auto n = obs.get_num_samples();
-    for (int i=0; i < n; ++i) {
-        double diff = (obs.get(0, i) - predictions[i].get_predictions()[0]);
-        mse += diff * diff;
-    }
-    mse /= n;
-    std::cout << "MSE: " << mse;
+    // Parse forest options and other command line options
+    ForestOptions forest_options = parse_forest_options(result);
 
+    // File management
+    std::string input_file = result.count("input_file") ? result["input_file"].as<std::string>() : throw std::runtime_error("Input file is required.");
+    std::string output_file = result.count("output_file") ? result["output_file"].as<std::string>() : "";
+    
+    std::string in_serial_name = result.count("load") ? result["load"].as<std::string>() : "";
+    std::string out_serial_name = result.count("write") ? result["write"].as<std::string>() : "";
+
+    // Column indices
+    uint outcome_index = result.count("outcome_index") ? result["outcome_index"].as<uint>() : throw std::runtime_error("Outcome index is required");
+
+    // Trains forest or loads from memory data
+    std::unique_ptr<Data> data(load_data(input_file));
+    Forest forest = in_serial_name.size() ? load_forest_from_file(in_serial_name) : train_regression_forest(std::move(data), forest_options, outcome_index);
+    
+    // Serialize forest, if applicable
+    if (!out_serial_name.empty()) save_forest_to_file(out_serial_name + ".forest", forest);
+
+    // Save predictions
+    if (!output_file.empty()) {
+        ForestPredictor predictor = ForestPredictors::regression_predictor(forest_options.get_num_threads(), forest_options.get_ci_group_size());
+        std::vector<Prediction> predictions = predictor.predict_oob(forest, data.get());
+        save_predictions_to_file(output_file, predictions);
+    }
+    
     return 0;
 }
